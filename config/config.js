7ee34a07d361aa92d6c6f25a16a4bbcbed89bd25
Luciano AAP <lucianoalmeidaap@gmail.com>
2022-01-15 18:41:28 -0300

    updates config

--- config/config.js
+++ config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: process.env.HOSTNAME || 'localhost',
     dialect: 'mysql',
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: process.env.HOSTNAME || 'localhost',
     dialect: 'mysql',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '',
     database: 'blogs_api',
-    host: process.env.HOSTNAME,
+    host: process.env.HOSTNAME || 'localhost',
     dialect: 'mysql',
   },
 };
